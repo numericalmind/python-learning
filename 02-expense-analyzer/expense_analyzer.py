969295d17bfaaa487cf06404c7a24f462f6ff4f8
Elanur Erboğa <elaheleng2121@gmail.com>
2026-08-18 20:32:37 +0000

    Clean expense analyzer and update visualization

--- 02-expense-analyzer/expense_analyzer.py
+++ 02-expense-analyzer/expense_analyzer.py
@@ -2,11 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 df = pd.read_csv("expenses.csv")
 print(df.head())
-df["Amount"].sum()
 print("Total_Expense:" ,df["Amount"].sum())
-df["Amount"].mean()
 print("Average_Expense:", df["Amount"].mean())
-df.groupby("Category")["Amount"].sum
 category_expenses = df.groupby("Category")["Amount"].sum()
 print("Category_Expenses:", category_expenses)
 highest_category = category_expenses.idxmax()
